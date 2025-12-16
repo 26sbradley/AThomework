3fe71bfad834be90cef9e9450d41fe279c8a6f90
26sbradley <26sbradley@derryfield.org>
2025-12-16 17:50:18 -0500

    Add files via upload

---
+++ Homework15/PartA.java
@@ -0,0 +1,8 @@
+//#1
+    // the answer is b)2
+
+//#2
+    // the answer is c)array[5] =1;
+
+//#3
+    // the answer is c)4
---
+++ Homework15/PartB.java
@@ -0,0 +1,18 @@
+public class PartB {
+    public static void main(String[] args) {
+
+        double [] numbers = { 2.2, 1.0, 3.7, 4.1, 88.0 };
+
+        double sum = 0.0;
+
+        for (double num : numbers) {
+            System.out.println(num);
+            sum += num;
+
+        }
+        double avg = sum / numbers.length;
+        System.out.println("The average is: " + avg);
+
+    }
+    
+}
